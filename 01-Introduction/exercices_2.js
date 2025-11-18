afbc9d67445aaa39f3f4ef9bdd79360b46d23b3d
karhel <karheltmarr@hotmail.fr>
2025-11-18 15:18:01 +0100

    Ex 2

--- 01-Introduction/exercices_2.js
+++ 01-Introduction/exercices_2.js
@@ -1,1 +1,15 @@
 // Exercice 2 - Nombre entier + somme
+
+// On demande un nombre entier à l'utilisateur
+let nbrUser = 0;
+do {
+	nbrUser = Number(prompt("Donnez-moi un nombre entier"));
+}
+while((nbrUser % 1 != 0) || isNaN(nbrUser));
+
+let nbrSum = 0;
+for(let i = 1; i <= nbrUser; i++) {
+	nbrSum += i;
+}
+
+console.log(`Somme de 1 à ${nbrUser} = ${nbrSum}`);
