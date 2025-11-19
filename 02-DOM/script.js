b95854312d91b0541aa2f1c64f0ef7b0662f13a8
karhel <karheltmarr@hotmail.fr>
2025-11-19 10:51:27 +0100

    Events - Introduction

--- 02-DOM/script.js
+++ 02-DOM/script.js
@@ -15,13 +15,40 @@ elTargetDiv.appendChild(elNewParagraph);
 
 const elInventoryList = document.getElementById('inventory');
 
-const elNewItem = document.createElement('li');
-elNewItem.textContent = "Armures";
-
-elInventoryList.appendChild(elNewItem);
-
 const elPotionItem = document.querySelector("#inventory li");
 elPotionItem.remove();
 
 // En une ligne :
 // document.querySelector("#inventory li").remove();
+
+
+const elButton = document.getElementById('btn-click');
+
+elButton.addEventListener('click', function(e) {
+	
+	console.log(e);
+	
+	// alert("Tu as cliqué!");
+	
+	const elNewItem = document.createElement('li');
+	elNewItem.textContent = "Armures";
+
+	elInventoryList.appendChild(elNewItem);
+});
+
+
+// Notation en fonction fléchée :
+/*
+elButton.addEventListener('click', () => {
+	alert("Tu as cliqué!");
+});
+*/
+
+// Notation complète
+/*
+function handleClick() {
+	alert("Tu as cliqué!");
+}
+
+elButton.addEventListener('click', handleClick);
+*/
