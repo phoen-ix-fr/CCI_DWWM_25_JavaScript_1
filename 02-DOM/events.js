9ba6214b513c4b8cd3cb56716d3e0c488a68205e
karhel <karheltmarr@hotmail.fr>
2025-11-19 13:43:36 +0100

    Events - Delete items in ul

--- 02-DOM/events.js
+++ 02-DOM/events.js
@@ -21,16 +21,42 @@ handleWindowResize();
 // Gestion de la liste
 const elItemList = document.getElementById('list');
 const elAddBtn 	 = document.getElementById('btn-add');
+const elDelBtn 	 = document.getElementById('btn-del');
 
 // On récupère le nombre de <li> déjà présents dans la liste + 1 pour le prochain ajout
-let i = elItemList.getElementsByTagName('li').length + 1;
+let i = elItemList.getElementsByTagName('li').length;
 
 // Ajout d'un nouvel item dans la liste
 elAddBtn.addEventListener('click', function(e) {
 	
 	const elNewItem = document.createElement('li');
-	elNewItem.textContent = `Item #${i++}`;
+	elNewItem.textContent = `Item #${++i}`;
 	
 	elItemList.append(elNewItem);
 });
 
+// Suppression du dernier item dans la liste
+elDelBtn.addEventListener('click', function(e) {
+	
+	// Test si la liste comporte au moins 1 élément à supprimer
+	// càd, si i est >= 1
+	if(i < 1) {
+		
+		alert("Liste vide");
+		return; //< Sortie de la fonction, en cas d'erreur
+	}
+	
+	const elItemToDelete = 
+		elItemList.getElementsByTagName('li')[i - 1];
+	
+	elItemToDelete.remove();
+	
+	i--;
+});
+
+
+// Incrémentations
+let j = 3;
+console.log(j);		// => 3
+console.log(j++);	// => 3, puis incrémentation (4)
+console.log(++j);	// => incrémentation, renvoi 5
