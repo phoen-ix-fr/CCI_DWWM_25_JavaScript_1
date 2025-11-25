69f92d1e71820c52c825dc33c9bfe15ebf2ca806
karhel <karheltmarr@hotmail.fr>
2025-11-25 12:52:33 +0100

    Chrono correction

---
+++ 05-Révisions/todo.js
@@ -0,0 +1,7 @@
+/* JavaScript ICI */
+// Récupération des éléments du domain
+
+// Association des évènements des clics sur les boutons
+// Pour l'ajout, on créer un nouvel élément dans le DOM qu'on ajoute à la liste
+
+// Pour la suppression on supprimer tous les éléments du DOM dans la liste
