b136f6f3a380ef9cc114c7c1371a6bebe6c198be
karhel <karheltmarr@hotmail.fr>
2025-11-26 11:04:45 +0100

    Remove button

--- 05-Révisions/todo.js
+++ 05-Révisions/todo.js
@@ -5,6 +5,8 @@ const elTaskInput 	= document.getElementById('taskInput');
 
 const arrStrTask	= [];	//< Tableau qui stockera la liste des tâches affichées
 
+let intTaskCount	= 0;
+
 // Association des évènements des clics sur les boutons
 
 // Pour l'ajout, on créer un nouvel élément dans le DOM qu'on ajoute à la liste
@@ -21,6 +23,8 @@ document.getElementById('addBtn').addEventListener('click', () => {
 	
 	elTaskItem.classList.add('task-item');	//< Rajoute la classe CSS au li	
 	
+	elTaskItem.dataset.taskIndex = intTaskCount;
+	
 	// <li><span class="task-text">....</span>...</li>
 	const elTaskSpan = document.createElement('span');
 	elTaskSpan.textContent = strTaskText;
@@ -32,15 +36,30 @@ document.getElementById('addBtn').addEventListener('click', () => {
 	elTaskButton.textContent = "Supprimer";
 	elTaskButton.classList.add('delete-btn');
 	
+	elTaskButton.addEventListener('click', (e) => {
+		const elTaskItemToDelete = e.target.parentElement;	
+		const intTaskIndex = elTaskItemToDelete.dataset.taskIndex;
+		
+		arrStrTask.splice(intTaskIndex, intTaskIndex);
+		elTaskItemToDelete.remove();
+	});
+	
 	elTaskItem.append(elTaskButton); //< Ajoute le bouton au <li>
 	
 	// Ajouter le <li> au <ul> : elTaskList
 	elTaskList.append(elTaskItem);
 	
 	arrStrTask.push(strTaskText); //< Ajout de la tâche dans le tableau
+	
+	elTaskInput.value = "";	//< Réinitialiser le texte de l'input à vide
+	
+	intTaskCount++;
 });
 
 // Pour la suppression on supprimer tous les éléments du DOM dans la liste
 document.getElementById('clearAllBtn').addEventListener('click', () => {
 	
+	elTaskList.innerHTML = "";
+	arrStrTask = [];
+	intTaskCount = 0;
 });
