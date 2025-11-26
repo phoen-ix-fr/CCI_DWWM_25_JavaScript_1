3ab05964251e761fcbcdc2971156f35cd2ccc853
karhel <karheltmarr@hotmail.fr>
2025-11-26 10:06:55 +0100

    Add Task on list

--- 05-Révisions/todo.js
+++ 05-Révisions/todo.js
@@ -3,13 +3,26 @@
 const elTaskList 	= document.getElementById('taskList');
 const elTaskInput 	= document.getElementById('taskInput');
 
+const arrStrTask	= [];	//< Tableau qui stockera la liste des tâches affichées
+
 // Association des évènements des clics sur les boutons
 
 // Pour l'ajout, on créer un nouvel élément dans le DOM qu'on ajoute à la liste
 document.getElementById('addBtn').addEventListener('click', () => {
 	
 	const strTaskText = elTaskInput.value; //< Récupération du texte de l'input
-	console.log(strTaskText);
+	
+	// Création d'un nouvel élément <li> dans ma liste
+	const elTaskItem = document.createElement('li');
+	elTaskItem.textContent = strTaskText; 	//< Affecte un contenu textuel simple
+											//< <li>Le texte de l'input</li>
+	
+	elTaskItem.classList.add('task-item');	//< Rajoute la classe CSS au li
+	
+	// Ajouter le <li> au <ul> : elTaskList
+	elTaskList.append(elTaskItem);
+	
+	arrStrTask.push(strTaskText); //< Ajout de la tâche dans le tableau
 });
 
 // Pour la suppression on supprimer tous les éléments du DOM dans la liste
