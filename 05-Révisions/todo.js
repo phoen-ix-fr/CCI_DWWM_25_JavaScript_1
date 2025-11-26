b0ea75cdd033b2ef46348b5af121d56ea8602160
karhel <karheltmarr@hotmail.fr>
2025-11-26 08:43:54 +0100

    Structure Todo

--- 05-Révisions/todo.js
+++ 05-Révisions/todo.js
@@ -1,7 +1,16 @@
 /* JavaScript ICI */
-// Récupération des éléments du domain
+// Récupération des éléments du DOM
+const elTaskList 	= document.getElementById('taskList');
+const elTaskInput 	= document.getElementById('taskInput');
 
 // Association des évènements des clics sur les boutons
+
 // Pour l'ajout, on créer un nouvel élément dans le DOM qu'on ajoute à la liste
+document.getElementById('addBtn').addEventListener('click', () => {
+	
+});
 
 // Pour la suppression on supprimer tous les éléments du DOM dans la liste
+document.getElementById('clearAllBtn').addEventListener('click', () => {
+	
+});
